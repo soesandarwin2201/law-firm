233f216d6d5ebb70bf3548a66e1aa5bfd648405f
Soe Sandar Win <soesandarwin2201@gmail.com>
2023-09-07 14:37:48 +0630

    create the about page

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,3 +1,9 @@
 import "../styles/main.scss";
 import "../script/typing";
 import "../script/couting";
+
+var swiper = new Swiper(".mySwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
