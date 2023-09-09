3e3e7bd729e6a694f02429ca1ce3281f62487999
Soe Sandar Win <soesandarwin2201@gmail.com>
2023-09-09 10:43:43 +0630

    create the service section

---
+++ assets/script/data.js
@@ -0,0 +1,52 @@
+const caseData = [
+  {
+    id: 1,
+    caseName: "Family case",
+    type: "Criminal Law",
+    img: "./assets/images/law.jpeg",
+    detail:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga possimus asperiores dolor tempora praesentium aperiam commodi, ex suscipit quaerat quam sed doloribus? Vero soluta aliquid fugiat officia accusantium est voluptate?",
+  },
+  {
+    id: 2,
+    caseName: "Affair Case",
+    type: "Civil Law",
+    img: "./assets/images/law.jpeg",
+    detail:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga possimus asperiores dolor tempora praesentium aperiam commodi, ex suscipit quaerat quam sed doloribus? Vero soluta aliquid fugiat officia accusantium est voluptate?",
+  },
+  {
+    id: 3,
+    caseName: "Affair Case",
+    type: "industrial Law",
+    img: "./assets/images/law.jpeg",
+    detail:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga possimus asperiores dolor tempora praesentium aperiam commodi, ex suscipit quaerat quam sed doloribus? Vero soluta aliquid fugiat officia accusantium est voluptate?",
+  },
+  {
+    id: 4,
+    caseName: "Affair Case",
+    type: "industrial Law",
+    img: "./assets/images/law.jpeg",
+    detail:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga possimus asperiores dolor tempora praesentium aperiam commodi, ex suscipit quaerat quam sed doloribus? Vero soluta aliquid fugiat officia accusantium est voluptate?",
+  },
+  {
+    id: 5,
+    caseName: "Affair Case",
+    type: "industrial Law",
+    img: "./assets/images/law.jpeg",
+    detail:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga possimus asperiores dolor tempora praesentium aperiam commodi, ex suscipit quaerat quam sed doloribus? Vero soluta aliquid fugiat officia accusantium est voluptate?",
+  },
+  {
+    id: 6,
+    caseName: "Affair Case",
+    type: "industrial Law",
+    img: "./assets/images/law.jpeg",
+    detail:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga possimus asperiores dolor tempora praesentium aperiam commodi, ex suscipit quaerat quam sed doloribus? Vero soluta aliquid fugiat officia accusantium est voluptate?",
+  },
+];
+
+export default caseData;
